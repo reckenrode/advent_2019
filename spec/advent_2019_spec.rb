4e202fa4229233f4c5916c242626717b2219c607
Randy Eckenrode <randy@largeandhighquality.com>
2019-12-08 20:30:57 -0500

    Drop the stub test

--- spec/advent_2019_spec.rb
+++ spec/advent_2019_spec.rb
@@ -12,8 +12,4 @@ RSpec.describe Advent2019 do
   it "has a version number" do
     expect(Advent2019::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
