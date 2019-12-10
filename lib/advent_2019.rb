eb5e0f0bb57a81c4d1be4f8eaa400816c72d4fd9
Randy Eckenrode <randy@largeandhighquality.com>
2019-12-09 22:39:39 -0500

    Add the entry point to the advent_2019 exe

--- lib/advent_2019.rb
+++ lib/advent_2019.rb
@@ -8,9 +8,40 @@
 # frozen_string_literal: true
 # encoding: utf-8
 
+require "advent_2019/day"
 require "advent_2019/version"
 
 module Advent2019
   class Error < StandardError; end
-  # Your code goes here...
+
+  # Helper method to convert file into a hash containing various ways to view
+  # the given file.
+  # @param [File] file
+  # @return a hash containing :raw access to or the :lines in the file
+  def self.get_file_views(file)
+    {
+      raw: file,
+      lines: file.each_line,
+    }
+  end
+
+  # Runs the Advent of Code 2019 solution indicated by the first element of
+  # argv.  The remaining elements of argv will be parsed as that day’s
+  # command-line arguments.
+  #
+  # @param [Array<String>] argv the command-line arguments
+  def self.main(argv)
+    Day.new
+    day = Day.days[argv.shift]
+    options = day.parse!(argv)
+    File.open(options.delete(:filename), "r") do |file|
+      readers = get_file_views(file)
+      day.run(readers, **options)
+    end
+  end
+
+  def self.show_help(msg)
+    puts msg
+    exit
+  end
 end
